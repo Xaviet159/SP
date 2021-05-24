0af3708ea3a6dd352239e370f4494e19efe650c5
Xavier <simonetxavier24@gmail.com>
2021-05-24 11:34:52 +0200

    update register

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegisterController extends AbstractController
+{
+    /**
+     * @Route("/register", name="register")
+     */
+    public function index(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder): Response
+    {
+        $user = new User;
+        
+        $form = $this->createForm(RegisterType::class, $user);
+
+            $form->handleRequest($request);
+            
+            if($form->isSubmitted()){  
+                $encoded = $encoder->encodePassword($user, $user->getPassword());
+                $user->setPassword($encoded);
+
+                $em->persist($user);
+                $em->flush();
+
+                return new RedirectResponse('/');
+            }
+
+            $formView = $form->createView();
+
+        return $this->render('register/index.html.twig', [
+            'formView' => $formView,
+        ]);
+    }
+}
---
+++ src/Form/RegisterType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('fullName', TextType::class, [
+                'label' => 'Nom et prénom',
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => ['class' => 'form-control']
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe',
+                'attr' => ['class' => 'form-control']
+            ])
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
